673c2b5a328079be4c5b75533c80b48f65baa2ba
xaviermiles <xaviermiles377@gmail.com>
2023-01-17 21:23:13 +1300

    Add ridge regression

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -2,7 +2,8 @@ use crate::traits::SupervisedModel;
 
 use anyhow;
 use nalgebra::{
-    self, allocator::Allocator, DefaultAllocator, Dim, OMatrix, OVector, RealField, SquareMatrix,
+    self, allocator::Allocator, DMatrix, DefaultAllocator, Dim, OMatrix, OVector, RealField,
+    SquareMatrix,
 };
 
 /// Simple linear regression using Ordinary Least Squares (OLS)
@@ -57,3 +58,65 @@ where
         }
     }
 }
+
+/// Ridge is linear regression with a penalty on the number of coefficients
+pub struct RidgeRegressor<T, R>
+where
+    T: RealField,
+    R: Dim,
+    DefaultAllocator: Allocator<T, R>,
+{
+    pub penalty: T,
+    pub coefficients: Option<OVector<T, R>>,
+}
+
+impl<T, R> RidgeRegressor<T, R>
+where
+    T: RealField,
+    R: Dim,
+    DefaultAllocator: Allocator<T, R>,
+{
+    pub fn new(penalty: T) -> Self {
+        // TODO: validate penalty > 0
+        Self {
+            penalty,
+            coefficients: None,
+        }
+    }
+}
+
+impl<T, R, C> SupervisedModel<OMatrix<T, R, C>, OVector<T, R>> for RidgeRegressor<T, C>
+where
+    T: RealField,
+    R: Dim,
+    C: Dim,
+    DefaultAllocator: Allocator<T, R, C>
+        + Allocator<T, R>
+        + Allocator<T, C>
+        + Allocator<T, C, R>
+        + Allocator<T, C, C>,
+    OMatrix<T, C, C>: std::ops::AddAssign<DMatrix<T>>,
+{
+    fn train(&mut self, inputs: &OMatrix<T, R, C>, outputs: &OVector<T, R>) -> anyhow::Result<()> {
+        let mut normal_matrix_inverse: SquareMatrix<T, C, _> = inputs.transpose() * inputs;
+        if !self.penalty.is_zero() {
+            let (n, _) = normal_matrix_inverse.shape();
+            let diagonal = DMatrix::from_diagonal_element(n, n, self.penalty.clone());
+            normal_matrix_inverse += diagonal;
+        }
+        if !normal_matrix_inverse.try_inverse_mut() {
+            panic!("The normal matrix is not invertible"); // TODO: return Error
+        }
+        let beta_hat = normal_matrix_inverse * inputs.transpose() * outputs;
+        self.coefficients = Some(beta_hat);
+        Ok(())
+    }
+
+    fn predict(&self, inputs: &OMatrix<T, R, C>) -> anyhow::Result<OVector<T, R>> {
+        match &self.coefficients {
+            Some(coefficient_estimates) => Ok(inputs * coefficient_estimates),
+            None => panic!("This model is not trained"), // TODO: return Error
+        }
+    }
+}
+
--- tests/linear_regression.rs
+++ tests/linear_regression.rs
@@ -1,7 +1,5 @@
-use core::panic;
-
 use nalgebra::{Matrix2, Matrix3x2, Vector};
-use slearning::linear_regression::OlsRegressor;
+use slearning::linear_regression::{OlsRegressor, RidgeRegressor};
 use slearning::SupervisedModel;
 
 #[test]
@@ -45,5 +43,50 @@ fn test_untrained_ols_fails_to_predict() {
     let test_input = Matrix3x2::from([[1.0, 2.0, 2.0], [3.0, 2.0, 3.0]]);
 
     let ols = OlsRegressor::default();
+    ols.predict(&test_input);
+}
+
+#[test]
+fn test_ridge_works() {
+    let penalty = 0.5;
+    let train_input = Matrix2::from([[1.0, 2.0], [3.0, 4.0]]);
+    let train_output = Vector::from([1.5, 3.5]);
+    let expected_coefficients = Vector::from([0.6883116883116889, 0.42857142857142855]);
+
+    let test_input = Matrix3x2::from([[1.0, 2.0, 2.0], [3.0, 2.0, 3.0]]);
+    let expected_prediction =
+        Vector::from([[1.9740259740259745, 2.233766233766235, 2.6623376623376633]]);
+
+    let mut ridge = RidgeRegressor::new(penalty);
+    assert_eq!(ridge.penalty, penalty);
+
+    if let Err(err) = ridge.train(&train_input, &train_output) {
+        panic!("Training caused error: {}", err);
+    }
+
+    match ridge.coefficients {
+        Some(actual_coefficients) => assert_eq!(actual_coefficients, expected_coefficients),
+        None => panic!("`coefficients` field is None"),
+    }
+
+    let prediction = ridge.predict(&test_input);
+    match prediction {
+        Ok(actual_prediction) => {
+            assert_eq!(
+                actual_prediction, expected_prediction,
+                "Prediction is incorrect"
+            )
+        }
+        Err(err) => panic!("Predicting caused error: {}", err),
+    }
+}
+
+#[test]
+#[should_panic(expected = "This model is not trained")]
+#[allow(unused_must_use)]
+fn test_untrained_ridge_fails_to_predict() {
+    let test_input = Matrix3x2::from([[1.0, 2.0, 2.0], [3.0, 2.0, 3.0]]);
+
+    let ols = RidgeRegressor::new(0.5);
     ols.predict(&test_input);
 }
