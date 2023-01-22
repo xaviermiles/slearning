7fa09e01c0ac9367dabdacc05158d15829ed30c9
xaviermiles <xaviermiles377@gmail.com>
2023-01-22 21:25:14 +1300

    Use test-case to parameterize ridge_works() test

--- tests/linear_regression.rs
+++ tests/linear_regression.rs
@@ -1,6 +1,7 @@
-use nalgebra::{Matrix2, Matrix3x2, Vector};
+use nalgebra::{Matrix2, Matrix3x2, Vector, Vector2, Vector3};
 use slearning::linear_regression::{OlsRegressor, RidgeRegressor};
 use slearning::{SLearningError, SupervisedModel};
+use test_case::test_case;
 
 #[test]
 fn ols_works() {
@@ -57,52 +58,31 @@ fn ols_fails_to_predict_when_untrained() {
     assert_eq!(actual, expected);
 }
 
-#[test]
-fn ridge_works() {
+#[test_case(
+    Matrix2::from([[1.0, 2.0], [3.0, 4.0]]),
+    Vector::from([1.5, 3.5]),
+    Vector::from([0.6883116883116889, 0.42857142857142855]),
+    Matrix3x2::from([[1.0, 2.0, 2.0], [3.0, 2.0, 3.0]]),
+    Vector::from([[1.9740259740259745, 2.233766233766235, 2.6623376623376633]]);
+    "normal"
+)]
+// Ridge regression (with non-zero penalty) is guaranteed to train with collinear input variables.
+#[test_case(
+    Matrix2::from([[1.0, 2.0], [2.0, 4.0]]),
+    Vector::from([1.5, 3.5]),
+    Vector::from([0.33333333333333404, 0.6666666666666672]),
+    Matrix3x2::from([[1.0, 2.0, 2.0], [3.0, 2.0, 3.0]]),
+    Vector::from([[2.3333333333333357, 2.0000000000000027, 2.6666666666666696]]);
+    "collinear input variables"
+)]
+fn ridge_works(
+    train_input: Matrix2<f64>,
+    train_output: Vector2<f64>,
+    expected_coefficients: Vector2<f64>,
+    test_input: Matrix3x2<f64>,
+    expected_prediction: Vector3<f64>,
+) {
     let penalty = 0.5;
-    let train_input = Matrix2::from([[1.0, 2.0], [3.0, 4.0]]);
-    let train_output = Vector::from([1.5, 3.5]);
-    let expected_coefficients = Vector::from([0.6883116883116889, 0.42857142857142855]);
-
-    let test_input = Matrix3x2::from([[1.0, 2.0, 2.0], [3.0, 2.0, 3.0]]);
-    let expected_prediction =
-        Vector::from([[1.9740259740259745, 2.233766233766235, 2.6623376623376633]]);
-
-    let mut ridge = RidgeRegressor::new(penalty).unwrap();
-    assert_eq!(ridge.penalty, penalty);
-
-    if let Err(err) = ridge.train(&train_input, &train_output) {
-        panic!("Training caused error: {}", err);
-    }
-
-    match ridge.coefficients {
-        Some(actual_coefficients) => assert_eq!(actual_coefficients, expected_coefficients),
-        None => panic!("`coefficients` field is None"),
-    }
-
-    let prediction = ridge.predict(&test_input);
-    match prediction {
-        Ok(actual_prediction) => {
-            assert_eq!(
-                actual_prediction, expected_prediction,
-                "Prediction is incorrect"
-            )
-        }
-        Err(err) => panic!("Predicting caused error: {}", err),
-    }
-}
-
-/// Test that ridge regression (with non-zero penalty) handles collinear input variables.
-#[test]
-fn ridge_works_with_collinear_input_variables() {
-    let penalty = 0.5;
-    let train_input = Matrix2::from([[1.0, 2.0], [2.0, 4.0]]);
-    let train_output = Vector::from([1.5, 3.5]);
-    let expected_coefficients = Vector::from([0.33333333333333404, 0.6666666666666672]);
-
-    let test_input = Matrix3x2::from([[1.0, 2.0, 2.0], [3.0, 2.0, 3.0]]);
-    let expected_prediction =
-        Vector::from([[2.3333333333333357, 2.0000000000000027, 2.6666666666666696]]);
 
     let mut ridge = RidgeRegressor::new(penalty).unwrap();
     assert_eq!(ridge.penalty, penalty);
