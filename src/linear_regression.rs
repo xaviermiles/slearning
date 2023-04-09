7de0e88daf533765c671a454de1b7e79e8b44095
Xavier Miles <xaviermiles377@gmail.com>
2023-04-09 15:58:56 +1200

    Change OLS/Ridge Regressors to use DMatrix/DVector instead
    This fixes a few problems:
    - previously the training data and predicting data had to be the exact
      same shape
    - you could not retrain the same model with a different number of
      coefficients or observations. *Maybe* coefficients being fixed for a
      given model struct makes sense, but not observations.
    - maybe more?

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -1,28 +1,17 @@
 use crate::traits::SupervisedModel;
 
 use crate::{SLearningError, SLearningResult};
-use nalgebra::DimName;
-use nalgebra::{
-    self, allocator::Allocator, DMatrix, DefaultAllocator, Dim, OMatrix, OVector, RealField,
-    SquareMatrix,
-};
+use nalgebra::{self, DMatrix, DVector, RealField};
 
-fn train_linear_regressor<T, R, C>(
-    inputs: &OMatrix<T, R, C>,
-    outputs: &OVector<T, R>,
+fn train_linear_regressor<T>(
+    inputs: &DMatrix<T>,
+    outputs: &DVector<T>,
     penalty: &T,
-) -> SLearningResult<OVector<T, C>>
+) -> SLearningResult<DVector<T>>
 where
     T: RealField,
-    R: Dim,
-    C: Dim + DimName,
-    DefaultAllocator: Allocator<T, R, C>
-        + Allocator<T, R>
-        + Allocator<T, C>
-        + Allocator<T, C, R>
-        + Allocator<T, C, C>,
 {
-    let mut normal_matrix_inverse: SquareMatrix<T, C, _> = inputs.transpose() * inputs;
+    let mut normal_matrix_inverse = inputs.transpose() * inputs;
     if !penalty.is_zero() {
         let (n, _) = normal_matrix_inverse.shape();
         let diagonal = DMatrix::from_diagonal_element(n, n, penalty.clone());
@@ -42,43 +31,32 @@ where
 /// Simple linear regression uses linear coefficients to model a single output variable as a
 /// function of one or more input variables.
 #[derive(Debug)]
-pub struct OlsRegressor<T, R>
+pub struct OlsRegressor<T>
 where
     T: RealField,
-    R: Dim,
-    DefaultAllocator: Allocator<T, R>,
 {
-    pub coefficients: Option<OVector<T, R>>,
+    pub coefficients: Option<DVector<T>>,
 }
 
-impl<T, R> Default for OlsRegressor<T, R>
+impl<T> Default for OlsRegressor<T>
 where
     T: RealField,
-    R: Dim,
-    DefaultAllocator: Allocator<T, R>,
 {
     fn default() -> Self {
         Self { coefficients: None }
     }
 }
 
-impl<T, R, C> SupervisedModel<OMatrix<T, R, C>, OVector<T, R>> for OlsRegressor<T, C>
+impl<T> SupervisedModel<T> for OlsRegressor<T>
 where
     T: RealField,
-    R: Dim,
-    C: Dim + DimName,
-    DefaultAllocator: Allocator<T, R, C>
-        + Allocator<T, R>
-        + Allocator<T, C>
-        + Allocator<T, C, R>
-        + Allocator<T, C, C>,
 {
-    fn train(&mut self, inputs: &OMatrix<T, R, C>, outputs: &OVector<T, R>) -> SLearningResult<()> {
+    fn train(&mut self, inputs: &DMatrix<T>, outputs: &DVector<T>) -> SLearningResult<()> {
         self.coefficients = Some(train_linear_regressor(inputs, outputs, &nalgebra::zero())?);
         Ok(())
     }
 
-    fn predict(&self, inputs: &OMatrix<T, R, C>) -> SLearningResult<OVector<T, R>> {
+    fn predict(&self, inputs: &DMatrix<T>) -> SLearningResult<DVector<T>> {
         match &self.coefficients {
             Some(coefficient_estimates) => Ok(inputs * coefficient_estimates),
             _ => Err(SLearningError::UntrainedModel),
@@ -91,21 +69,17 @@ where
 /// The penalty is a non-negative real value. A penalty of zero means that ridge regression is
 /// equivalent to simple linear regression.
 #[derive(Debug)]
-pub struct RidgeRegressor<T, R>
+pub struct RidgeRegressor<T>
 where
     T: RealField,
-    R: Dim,
-    DefaultAllocator: Allocator<T, R>,
 {
     pub penalty: T,
-    pub coefficients: Option<OVector<T, R>>,
+    pub coefficients: Option<DVector<T>>,
 }
 
-impl<T, R> RidgeRegressor<T, R>
+impl<T> RidgeRegressor<T>
 where
     T: RealField,
-    R: Dim,
-    DefaultAllocator: Allocator<T, R>,
 {
     pub fn new(penalty: T) -> SLearningResult<Self> {
         if penalty.is_negative() {
@@ -120,24 +94,16 @@ where
     }
 }
 
-impl<T, R, C> SupervisedModel<OMatrix<T, R, C>, OVector<T, R>> for RidgeRegressor<T, C>
+impl<T> SupervisedModel<T> for RidgeRegressor<T>
 where
     T: RealField,
-    R: Dim,
-    C: Dim + DimName,
-    DefaultAllocator: Allocator<T, R, C>
-        + Allocator<T, R>
-        + Allocator<T, C>
-        + Allocator<T, C, R>
-        + Allocator<T, C, C>,
-    OMatrix<T, C, C>: std::ops::AddAssign<DMatrix<T>>,
 {
-    fn train(&mut self, inputs: &OMatrix<T, R, C>, outputs: &OVector<T, R>) -> SLearningResult<()> {
+    fn train(&mut self, inputs: &DMatrix<T>, outputs: &DVector<T>) -> SLearningResult<()> {
         self.coefficients = Some(train_linear_regressor(inputs, outputs, &self.penalty)?);
         Ok(())
     }
 
-    fn predict(&self, inputs: &OMatrix<T, R, C>) -> SLearningResult<OVector<T, R>> {
+    fn predict(&self, inputs: &DMatrix<T>) -> SLearningResult<DVector<T>> {
         match &self.coefficients {
             Some(coefficient_estimates) => Ok(inputs * coefficient_estimates),
             None => Err(SLearningError::UntrainedModel),
